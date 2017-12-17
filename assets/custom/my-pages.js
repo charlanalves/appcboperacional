59081716503549bc8b8dfc94df7c146246b804c9
Charlan Santos <charlan.job@gmail.com>
2017-12-17 19:00:06 -0200

    Revert "PDV: final"
    This reverts commit 69fd9ed468f697f41f806aa9681f6dc4f8789375.

--- assets/custom/my-pages.js
+++ assets/custom/my-pages.js
@@ -21,7 +21,6 @@ class LoadPage {
             cbDia = 0,
             vlrTotalPromocao = 0,
             vlrTotalCompra = 0,
-            vlrCbTotal = 0,
             itensPedido = [];
             
 
@@ -103,7 +102,6 @@ class LoadPage {
                 var vlrTotalCb = 0,
                     vlrUnidadePromocao = 0,
                     idPromocao = 0,
-                    namePromocao = '',
                     qtdSelecionada = 0;
             
                 $('#listPromocoes li .list-promocoes').each(function(){
@@ -124,11 +122,8 @@ class LoadPage {
                         // id da promocao
                         idPromocao = $($(this).find('div')[0]).data('promocao');
 
-                        // nome da promocao
-                        namePromocao = $(this).find('.name').text();
-
                         // itens do pedido
-                        itensPedido.push({promocao: idPromocao, nome: namePromocao, qtd: qtdSelecionada, vlr_unidade: vlrUnidadePromocao});
+                        itensPedido.push({promocao: idPromocao, qtd: qtdSelecionada, vlr_unidade: vlrUnidadePromocao});
 
                     }
                     
@@ -140,8 +135,7 @@ class LoadPage {
             var calcTotalCb = function () {
                 var vlrCbDia = Util.formatNumberBR($('.detalheCompra .total-cb-dia').text()),
                     vlrCbPromocao = Util.formatNumberBR($('.detalheCompra .total-cb-promocao').text());
-                    vlrCbTotal = vlrCbDia + vlrCbPromocao;
-                $('.detalheCompra .total-cb').text(Util.formatNumber(vlrCbTotal));
+                $('.detalheCompra .total-cb').text(Util.formatNumber(vlrCbDia + vlrCbPromocao));
             };
 
             // calcula total da promocao
@@ -156,10 +150,6 @@ class LoadPage {
                 }
             };
             
-            $(objFormFinalizar['busca_cpf']).keyup(function () {
-                $(objFormFinalizar['forma_pagamento']).html('');
-            });
-            
             // btn pesquisar cliente
             formFinalizar.find('#btn-busca-cliente-pdv').on('click', function () {
                 var CPF = formFinalizar.find('input[name=busca_cpf]').val();
@@ -173,6 +163,7 @@ class LoadPage {
                             // nome do cliente
                             $('#erro-busca-cpf').html(a.cliente.name).removeClass('form-erro').show();
                             // opcoes de pagamento
+                            $(objFormFinalizar['forma_pagamento']).html('');
                             objFormFinalizar.addOptionsSelect('forma_pagamento', a.formasPagamento);
                         }
                         $(btnHtml).removeAttr('disabled');
